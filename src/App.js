1da66d378cc24e64ac58be747ae73237caf661e8
aureliane dor <aureliane.dor@gmail.com>
2022-08-09 20:50:01 +0200

    game over screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {createContext, useContext, useState, useEffect} from "react";
-import {isDisabled} from "@testing-library/user-event/dist/utils";
 
 const PlayerListContext = createContext([undefined]);
 
@@ -8,182 +7,219 @@ const PlayerListContext = createContext([undefined]);
 const LOCAL_STORAGE_KEY = "player-list";
 
 // Provider qui permet d'acceder à la liste des joueurs dans tous les components sans les passer dans des props
-function PlayerListProvider ({ children }) {
+function PlayerListProvider({children}) {
 
-  const [playerList, setPlayerList] = useState([]);
+	const [playerList, setPlayerList] = useState([{
+		name: "aureliane",
+		score: 0
+	}, {
+		name: "clement",
+		score: 0
+	}, {
+		name: "toto",
+		score: 0
+	}, {
+		name: "pepette",
+		score: 0
+	}, {
+		name: "jon",
+		score: 0
+	}]);
 
-  //Stocker la liste des joueurs dans variable local LOCAL_STORAGE_KEY
+	//Stocker la liste des joueurs dans variable local LOCAL_STORAGE_KEY
 
-  useEffect(() => {
-    const storagePlayerList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storagePlayerList) {
-      setPlayerList(storagePlayerList);
-    }
-  }, [])
+	useEffect(() => {
+		const storagePlayerList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+		if (storagePlayerList) {
+			setPlayerList(storagePlayerList);
+		}
+	}, [])
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(playerList));
-  }, [playerList])
+	useEffect(() => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(playerList));
+	}, [playerList])
 
-  return (
-    <PlayerListContext.Provider value={{playerList, setPlayerList}}>
-        {children}
-    </PlayerListContext.Provider>
-  )
+	return (
+		<PlayerListContext.Provider value={{playerList, setPlayerList}}>
+			{children}
+		</PlayerListContext.Provider>
+	)
 }
 
 function AddPlayer(props) {
 
-  const [name, setName] = useState("");
-  const {playerList, setPlayerList}  = useContext(PlayerListContext);
+	const [name, setName] = useState("");
+	const {playerList, setPlayerList} = useContext(PlayerListContext);
 
-  const addPlayer = () => {
-    if (name !== "") {
-      setPlayerList((list) => [...list, {name, score: 0}])
-      setName("")
-    }
+	const addPlayer = () => {
+		if (name !== "") {
+			setPlayerList((list) => [...list, {name, score: 0}])
+			setName("")
+		}
 
-  }
-  return(
-    <div>
-      <input type="text" name="name" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)} />
-      <button onClick={addPlayer}>Ajouter un joueur</button>
-    </div>
-  )
+	}
+	return (
+		<div>
+			<input type="text" name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+			<button onClick={addPlayer}>Ajouter un joueur</button>
+		</div>
+	)
 
 }
+
 function PlayerList() {
-  const {playerList, setPlayerList}  = useContext(PlayerListContext);
+	const {playerList, setPlayerList} = useContext(PlayerListContext);
 
+	// Supprimer un joueur de la liste
 
+	const deleteName = (index) => {
+		const newList = playerList;
+		newList.splice(index, 1);
+		setPlayerList((list) => [...newList]);
+	}
 
-  // Supprimer un joueur de la liste
-
-  const deleteName = (index) => {
-    const newList = playerList;
-    newList.splice(index,1);
-    setPlayerList((list) => [...newList]);
-  }
-
-  return (
-    <div>
-      {
-        playerList.map((player, index)=>
-          <div key={index} className="center">
-            <div className="flex">
-              <span>{player.name}</span>
-              <button onClick={()=>deleteName(index)}>x</button>
-            </div>
-          </div>)
-      }
-    </div>
-  )
-
+	return (
+		<div>
+			{
+				playerList.map((player, index) =>
+					<div key={index} className="center">
+						<div className="flex">
+							<span>{player.name}</span>
+							<button onClick={() => deleteName(index)}>x</button>
+						</div>
+					</div>)
+			}
+		</div>
+	)
 
 
 }
+
 function PrepareGame(props) {
-  const {playerList, setPlayerList}  = useContext(PlayerListContext);
+	const {playerList, setPlayerList} = useContext(PlayerListContext);
 
-
-  return (
-    <>
-      { playerList.length < 5 ? <AddPlayer /> : null}
-      <PlayerList />
-      <button onClick={() => { props.onClick(); }}>Commencer la partie</button>
-    </>  );
+	return (
+		<>
+			{playerList.length < 5 ? <AddPlayer/> : null}
+			<PlayerList/>
+			<button onClick={() => {
+				props.onClick();
+			}}>Commencer la partie
+			</button>
+		</>);
 }
 
 
 function Game(props) {
-  const [arrivedPlayers, setArrivedPlayers] = useState([]);
-  const[round, setRound] = useState(2);
+	const {playerList, setPlayerList} = useContext(PlayerListContext);
+	const [arrivedPlayers, setArrivedPlayers] = useState([]);
+	const [round, setRound] = useState(1);
+	const [count, setCount] = useState(0);
 
 
-  return (
-    <>
-      <CardsPlayer onClick={props.onClick} arrivedPlayers={{arrivedPlayers, setArrivedPlayers}} round={{round, setRound}}/>
-      <ScoreTable arrivedPlayers={{arrivedPlayers, setArrivedPlayers}} round={{round, setRound}}/>
-    </>
-  );
+	return (
+		<>
+			{count === 3 ? playerList.map((player, score) =>
+					<div key={score} className="center">
+						<div className="flex">
+							<p>{player.name}</p>
+							<p>{player.score}</p>
+						</div>
+					</div>
+				) :
+				<div>
+					<CardsPlayer onClick={props.onClick} arrivedPlayers={{arrivedPlayers, setArrivedPlayers}}
+											 round={{round, setRound}}/>
+					<ScoreTable arrivedPlayers={{arrivedPlayers, setArrivedPlayers}} round={{round, setRound}}
+											count={{count, setCount}}/>
+				</div>
+			}
+		</>
+	)
 }
 
 function CardsPlayer(props) {
-  const {arrivedPlayers, setArrivedPlayers} = props.arrivedPlayers
-  const {playerList, setPlayerList}  = useContext(PlayerListContext);
+	const {arrivedPlayers, setArrivedPlayers} = props.arrivedPlayers
+	const {playerList, setPlayerList} = useContext(PlayerListContext);
 
 
-  //cette fonction ajoute l'index du joueur dans une liste par ordre d'arrivé
-  function FinalList(index) {
-    playerList[index].score += ((playerList.length - arrivedPlayers.length) * props.round.round)
+	//cette fonction ajoute l'index du joueur dans une liste par ordre d'arrivé
+	function FinalList(index) {
+		playerList[index].score += ((playerList.length - arrivedPlayers.length) * props.round.round)
 
-    setArrivedPlayers([...arrivedPlayers, playerList[index]])
+		setArrivedPlayers([...arrivedPlayers, playerList[index]])
 
-    setPlayerList(playerList)
-    }
+		setPlayerList(playerList)
+	}
 
 
-  return (
-      <>
-        {
-          playerList.map(
-            (player, index) =>
-            <div key={index} className="center">
-              <button disabled={arrivedPlayers.indexOf(player) !== -1}
-                      onClick={() =>
-                      {FinalList(index);
-                        props.onClick();
-                      }
-              }
-              >
-                {player.name}
-              </button>
-            </div>
-          )
-        }
-      </>
-  )
+	return (
+		<>
+			{
+				playerList.map(
+					(player, index) =>
+						<div key={index} className="center">
+							<button disabled={arrivedPlayers.indexOf(player) !== -1}
+											onClick={() => {
+												FinalList(index);
+												props.onClick();
+											}
+											}
+							>
+								{player.name}
+							</button>
+						</div>
+				)
+			}
+		</>
+	)
 }
 
-function ScoreTable (props) {
-  const {arrivedPlayers, setArrivedPlayers} = props.arrivedPlayers;
+function ScoreTable(props) {
+	const {playerList, setPlayerList} = useContext(PlayerListContext);
+	const {arrivedPlayers, setArrivedPlayers} = props.arrivedPlayers;
+	const {round, setRound} = props.round;
+	const {count, setCount} = props.count;
 
-  function incrementRound () {
-    props.round.setRound(props.round.round +1)
-    setArrivedPlayers([])
-  }
+	function incrementRound() {
+		setRound(round + 1)
+		setArrivedPlayers([])
+		setCount(count + 1)
+	}
 
-  console.log(props.round.round);
-  return (
-    <div>
-      {
-        arrivedPlayers.map((player, index)=>
-          <div key={index} className="center">
-            <div className="flex">
-              <span>{player.name}</span>
-              <span>{player.score}</span>
-            </div>
-          </div>)
-      }
-      <button onClick={incrementRound}>Terminer la manche</button>
-    </div>
-  )
+	return (
+		<div>
+			{
+				arrivedPlayers.map((player, index) =>
+					<div key={index} className="center">
+						<div className="flex">
+							<span>{player.name}</span>
+							<span>{player.score}</span>
+						</div>
+					</div>)
+			}
+			<button onClick={incrementRound}
+							disabled={playerList.length !== arrivedPlayers.length}
+			>Terminer la manche
+			</button>
+		</div>
+	)
 }
 
 
 function App() {
-  const [showContent, setShowContent] = useState(true);
-  const onClick = () => setShowContent(false);
+	const [showContent, setShowContent] = useState(true);
+	const show = () => setShowContent(false);
 
-  return (
-    <div className="App">
-      <PlayerListProvider>
-        <h1>Vélonimo</h1>
+	return (
+		<div className="App">
+			<PlayerListProvider>
+				<h1>Vélonimo</h1>
 
-        { showContent ? <PrepareGame onClick={onClick} />:  <Game onClick={onClick} showContent={showContent} />}
-      </PlayerListProvider>
-    </div>
-  )
+				{showContent ? <PrepareGame onClick={show}/> : <Game onClick={show} showContent={showContent}/>}
+			</PlayerListProvider>
+		</div>
+	)
 }
 
 export default App;
